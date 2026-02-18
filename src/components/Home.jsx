f6fba346c9a84dec5132427ef78fea723e5af586
= <lukejmullins@gmail.com>
2026-02-18 11:33:13 -0500

    Finish Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,17 @@
+import "../styles/home.css";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div className="welcome">
+        <h1>Welcome to Fake Store!</h1>
+      </div>
+      <div>
+        <p className="description">
+          Browser are store to find a variety of fake items! Navigate by using
+          the links above.
+        </p>
+      </div>
+    </div>
+  );
 }
